b0cd7b9c25a335f5bfcba6a987c405897541950b
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2023-05-08 11:23:19 -0600

    add w2d5 APIs fetch vs axios

---
+++ W2D1_APIS_useEffect/superhero_api/src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  // state vars
+  const [superheroes, setSuperHeroes] = useState([])
+
+  // VANILLA JS fetch
+  const fetchSuperHeroes = () => {
+    // console.log("fetch")
+    // FETCH RETURNS A Promise
+    fetch("https://akabab.github.io/superhero-api/api/all.json")
+      .then((serverResponse) => {
+        // console.log(serverResponse)
+        return serverResponse.json()
+      })
+      .then((jsonResponse) => {
+        console.log(jsonResponse)
+        // set state here where I have access
+        setSuperHeroes(jsonResponse)
+      })
+      .catch((errObj) => console.log("❌ GOT AND ERROR =>", errObj))
+  }
+
+  // AXIOS GET
+  const axiosSuperHeroes = () => {
+    axios.get("https://akabab.github.io/superhero-api/api/all.json")
+      .then((serverRes) => {
+        console.log(serverRes.data)
+        // ! AXIOS WRAPS its response inside a 'data' key
+        setSuperHeroes(serverRes.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  return (
+    <div className="App">
+      <h1>SUPERHERO API 🦸‍♀️🦸‍♂️</h1>
+
+      <button onClick={fetchSuperHeroes}>fetch superheroes</button> |
+      <button onClick={axiosSuperHeroes}>AXIOS 💪 superheroes</button>
+      <hr />
+
+      {/* {JSON.stringify(superheroes)} */}
+
+      <table>
+        <thead>
+          <tr>
+            <th>image</th>
+            <th>hero name</th>
+            <th>full name</th>
+            <th>publisher</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            superheroes.map((hero) => {
+              // console.log(hero.name)
+              return (
+                <tr key={hero.id}>
+                  <td><img src={hero.images.sm} alt="some hero" width="80px" /></td>
+                  <td>{hero.name}</td>
+                  <td>{hero.biography.fullName ? hero.biography.fullName : "UNKNOWN"}</td>
+                  <td>{hero.biography.publisher}</td>
+                </tr>
+              )
+            })
+          }
+
+
+        </tbody>
+
+      </table>
+    </div>
+  );
+}
+
+export default App;
